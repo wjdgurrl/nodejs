51e85fabf82001f5adb1c9e0788befa9481adbd6
gurrl <gurrl8356@gmail.com>
2025-01-15 00:00:07 +0900

    passport 로그인 까지 구현

--- server.js
+++ server.js
@@ -23,6 +23,22 @@ const { ObjectId } = require('mongodb')
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
+//passport라이브러리 설정
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+
+app.use(passport.initialize())
+app.use(session({
+    secret: '암호화에 쓸 비번', //암호화 키
+    resave : false, //유저가 요청 날릴때마다 session데이터를 갱신할지 여부(false추천)
+    saveUninitialized : false //유저가 로그인안해도 세션을 저장해둘지 여부(false추천)
+}))
+
+app.use(passport.session())
+
+
+
 app.use(express.static(__dirname + '/public')) //css 파일 폴더 등록
 
 app.listen(8080, () => {
@@ -95,4 +111,65 @@ app.put('/edit',async (req,res)=>{
         { _id : new ObjectId('수정할 document _id') },
         { $inc: { like : 1 } } //$inc는 +값을 해줌 음수면 마이너스
     )*/
+})
+
+app.delete('/delete', async (req, res) => {
+    //console.log(req.query.docid)
+    await db.collection('post').deleteOne({ _id : new ObjectId(req.query.docid)})
+    res.send('삭제완료')
+})
+
+//페이지네이션
+//파라미터 구현
+/*
+app.get('/list/:page', async (req, res) => {
+    //let result = await db.collection('post').find().limit(5).toArray() //5개만 찾아와줌
+    let result = await db.collection('post').find()
+        .skip((req.params.page - 1)*5).limit(5).toArray() //맨 앞에 5개를 스킵하고 5개를 찾아옴
+    res.render('list.ejs',{글목록: result}) //result데이터를 글목록이라는 이름으로 데이터전달
+})*/
+
+//다음버튼으로 구현
+app.get('/list/:page', async (req, res) => {
+    //let result = await db.collection('post').find().limit(5).toArray() //5개만 찾아와줌
+    let result = await db.collection('post').find()
+        .skip((req.params.page - 1)*5).limit(5).toArray() //맨 앞에 5개를 스킵하고 5개를 찾아옴
+    res.render('list.ejs',{글목록: result}) //result데이터를 글목록이라는 이름으로 데이터전달
+})
+//페이지네이션으로 구성하고 다음버튼을 작동
+app.get('/list/next/:id', async (req, res) => {
+    let result = await db.collection('post')
+        .find({ _id : {$gt : new ObjectId(req.params.id)}})
+        .limit(5).toArray()
+    res.render('list.ejs', { 글목록 : result })
+})
+
+//로그인
+app.get('/login', async (req,res) =>{
+    res.render('login.ejs')
+})
+
+//passport 라이브러리
+passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
+    let result = await db.collection('user').findOne({ username : 입력한아이디})
+    if (!result) {
+        return cb(null, false, { message: '아이디 DB에 없음' })
+    }
+    if (result.password == 입력한비번) {
+        return cb(null, result)
+    } else {
+        return cb(null, false, { message: '비번불일치' });
+    }
+}))
+//사용시 passport.authenticate('local')을 api안에서 쓰면 됨
+
+app.post('/login', async (req,res,next) =>{
+    passport.authenticate('local', (error, user, info) => {
+        if (error) return res.status(500).json(error)
+        if (!user) return res.status(401).json(info.message)
+        req.logIn(user, (err) => {
+            if (err) return next(err)
+            res.redirect('/')
+        })
+    })(req, res, next)
 })
